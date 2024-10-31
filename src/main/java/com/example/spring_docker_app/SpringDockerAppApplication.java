1d05f35c0ff61e83368a0e586f7610adb5859163
Mohd Sakib Shaikh <mohd.sakib.shaikh@ibm.com>
2024-10-31 17:17:01 +0530

    docker config with get api

---
+++ src/main/java/com/example/spring_docker_app/SpringDockerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_docker_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerAppApplication.class, args);
+	}
+
+}
